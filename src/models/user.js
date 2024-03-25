9cfa53b70992375a8c50512b9a187c48f1b36942
nilpeep <niluferk038@gmail.com>
2024-03-25 21:37:46 +0300

    user model

--- src/models/user.js
+++ src/models/user.js
@@ -4,3 +4,36 @@
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
+
+// user model:
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    isActive:{
+        type: Boolean,
+        default: true,
+    }
+}, {
+    collection: 'users',
+    timestamps: true
+})
+
+// Model:
+
+module.exports = mongoose.model('User', userSchema)
