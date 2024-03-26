67e45d916daa23c6614d1991d9c5ecb590d47378
nilpeep <niluferk038@gmail.com>
2024-03-26 15:09:40 +0300

    user router

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,19 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 
+const user = require('../controllers/user')
+
+router.route('/', )
+    .get(user.list)
+    .post(user.create)
+
+router.route('/:id')
+    .get(user.read)
+    .put(user.update)
+    .delete(user.delete)
+    .patch(user.update)
+    
+
+
 /* ------------------------------------------------------- */
 module.exports = router
